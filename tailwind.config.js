e939dd0f68fa5c468781c1e4be01973f0d609256
aratmany <arat.mda@gmail.com>
2024-10-04 02:48:07 +0300

    add next js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+const config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter, sans-serif', { fontFeatureSettings: '"cv11"' }],
+      },
       colors: {
-        black: '#000'
+        black: '#000',
       },
       zIndex: {
         1000: '1000', // 0000-1999: основной контент
@@ -16,10 +22,12 @@ export default {
         7000: '7000',
         8000: '8000', // 8000-8999: модальные окна
         9000: '9000', // 9000-9999: уведомления
-        9999: '9999' // 9000-9999: уведомления
-      }
-    }
+        9999: '9999', // 9000-9999: уведомления
+      },
+    },
   },
   plugins: [],
-  darkMode: 'class'
-};
+  darkMode: 'class',
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = config
